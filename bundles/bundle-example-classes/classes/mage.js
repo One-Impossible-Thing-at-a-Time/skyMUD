7ce16e7230556f34745efe5ae370437cc4c7a2f2
Lucas Steuber <78874798+lukeslp@users.noreply.github.com>
2024-02-28 16:38:14 -0800

    Housekeeping push

---
+++ bundles/bundle-example-classes/classes/mage.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * See warrior.js for more on classes.
+ */
+module.exports = {
+  name: "shitposter",
+  description: "Shitposters are the Mages of bluesky.",
+  abilityTable: {
+    5: { spells: ["fireball"] },
+  },
+
+  setupPlayer: (state, player) => {
+    const mana = state.AttributeFactory.create("mana", 100);
+    player.addAttribute(mana);
+    player.prompt =
+      "[ %health.current%/%health.max% <b>hp</b> %mana.current%/%mana.max% <b>mana</b> ]";
+  },
+};
---
+++ bundles/bundle-example-classes/classes/paladin.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  name: 'sincereposter',
+  description: 'Sincereposters are the Paladins of bluesky.',
+
+  abilityTable: {
+    3: { skills: ['judge'] },
+    5: { skills: ['plea'] },
+    7: { skills: ['smite'] },
+  },
+
+  setupPlayer: (state, player) => {
+    // Paladins use Favor, with a max of 10. Favor is a generated resource and returns to 0 when out of combat
+    const favor = state.AttributeFactory.create('favor', 10, -10);
+    player.addAttribute(favor);
+    player.prompt = '[ <b><red>%health.current%</red></b>/<red>%health.max%</red> <b>hp</b> <b><yellow>%favor.current%</yellow></b>/<yellow>%favor.max%</yellow> <b>favor</b> ]';
+  }
+};
